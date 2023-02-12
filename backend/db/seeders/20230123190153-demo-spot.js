520e90bf9add878c49ff10919f13d9d478d3f2e9
Hamilton <hamiltontruong@gmail.com>
2023-02-12 14:48:14 -0800

    Update seed data for spots

--- backend/db/seeders/20230123190153-demo-spot.js
+++ backend/db/seeders/20230123190153-demo-spot.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
 let options = {};
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   options.schema = process.env.SCHEMA;
 }
 
@@ -9,78 +9,105 @@ if (process.env.NODE_ENV === 'production') {
 // similar to week 11's pa with players, sport, team, etc
 // const { Spot, User } = require('../models')
 
-// tried to make it dynamic via similar code from wk11, 
+// tried to make it dynamic via similar code from wk11,
 // but for now, move on with direct ownerId input
-  // for (let spot of spots) {
-  //   const { firstName } = spot
-  //   const findOwner = await User.findOne({
-  //     where: firstName
-  //   })
-  //   await Spot.create({
-  //     ownerId: findOwner.id,
-  //   })
-  // }
+// for (let spot of spots) {
+//   const { firstName } = spot
+//   const findOwner = await User.findOne({
+//     where: firstName
+//   })
+//   await Spot.create({
+//     ownerId: findOwner.id,
+//   })
+// }
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    options.tableName = 'Spots';
-    return queryInterface.bulkInsert(options, [
-      {
-        ownerId: 1,
-        address: '123 Disney Lane',
-        city: 'San Francisco',
-        state: 'California',
-        country: 'United States of America',
-        lat: 37.7645358,
-        lng: -122.4730327,
-        name: "App Academy",
-        description: "Place where web developers are created",
-        price: 123,
-      },
-      {
-        ownerId: 1,
-        address: '321 Disney Lane',
-        city: 'Franciso San',
-        state: 'California',
-        country: 'USA',
-        lat: 37.7645358,
-        lng: -122.4730327,
-        name: "Apps Academy",
-        description: "Test additional spot associated to ownerId",
-        price: 321,
-      },
-      {
-        ownerId: 2,
-        address: '456 Disney World',
-        city: 'Orlando',
-        state: 'Florida',
-        country: 'United States of America',
-        lat: 28.3772,
-        lng: 81.5707,
-        name: "Disney World",
-        description: "A wonderful resort",
-        price: 456,
-      },
-      {
-        ownerId: 3,
-        address: '789 Disney Sea',
-        city: 'Urayasu',
-        state: 'Tokyo',
-        country: 'Japan',
-        lat: 35.6267,
-        lng: 139.8851,
-        name: "Disney Sea",
-        description: "A nautical adventure",
-        price: 456,
-      }
-    ], {});
+  async up(queryInterface, Sequelize) {
+    options.tableName = "Spots";
+    return queryInterface.bulkInsert(
+      options,
+      [
+        {
+          ownerId: 1,
+          address: "123 Castle Road",
+          city: "San Francisco",
+          state: "California",
+          country: "United States of America",
+          lat: 37.7645358,
+          lng: -122.4730327,
+          name: "Castle in the Sky",
+          description: "Explore a magical aerial adventure",
+          price: 123,
+        },
+        {
+          ownerId: 1,
+          address: "456 Port City",
+          city: "San Francisco",
+          state: "California",
+          country: "USA",
+          lat: 37.7645358,
+          lng: -122.4730327,
+          name: "Port City Koriko",
+          description: "Enjoy the warmth of fresh bread and friendly neighbors",
+          price: 456,
+        },
+        {
+          ownerId: 2,
+          address: "789 Camphor Lane",
+          city: "Orlando",
+          state: "Florida",
+          country: "United States of America",
+          lat: 28.3772,
+          lng: 81.5707,
+          name: "Camphor Tree",
+          description: "Discover a hidden, magical retreat",
+          price: 789,
+        },
+        {
+          ownerId: 2,
+          address: "10 Secret Garden",
+          city: "Urayasu",
+          state: "Tokyo",
+          country: "Japan",
+          lat: 35.6267,
+          lng: 139.8851,
+          name: "Secret Garden",
+          description: "Delve into a tucked away, lush garden",
+          price: 10,
+        },
+        {
+          ownerId: 3,
+          address: "136 Tomocho",
+          city: "Fukuyama",
+          state: "Hiroshima",
+          country: "Japan",
+          lat: 35.6267,
+          lng: 139.8851,
+          name: "Old Port Town",
+          description: "A quaint and charming port town",
+          price: 136,
+        },
+        {
+          ownerId: 3,
+          address: "56 Dogo",
+          city: "Matsuyama",
+          state: "Ehime",
+          country: "Japan",
+          lat: 35.6267,
+          lng: 139.8851,
+          name: "The Bath House",
+          description: "Replenish your spirit at The Bath House",
+          price: 456,
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    options.tableName = 'Spots';
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Spots";
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
-    }, {});
-  }
+    return queryInterface.bulkDelete(options, {}, {});
+  },
 };
